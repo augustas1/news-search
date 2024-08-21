d1a30eb2e4023f65b07cd3fc628db258c7e85673
augustas1 <augustas.skaburskas@gmail.com>
2024-08-21 13:20:44 +0300

    setup React app

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='max-w-4xl my-8 mx-auto px-8'>
+      <input autoFocus className='px-4 py-2 border rounded-full w-full bg-gray-200 focus:outline-none focus:ring-1' />
+    </div>
+  )
+}
+
+export default App
